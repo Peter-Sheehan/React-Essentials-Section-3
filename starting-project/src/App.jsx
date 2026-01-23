a0aef21d47978d4136c61bb900df3d1244c5f29a
PeterSheehan <sheehanpeter01@gmail.com>
2026-01-23 23:17:37 +0000

    Added feature pages to custom components

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,69 +1,17 @@
-import { useState } from 'react';
-
-import { CORE_CONCEPTS, EXAMPLES } from './data.js';
-import CoreConcept from './Components/CoreConcept/CoreConcept.jsx';
+import CoreConcepts from './Components/CoreConcept/CoreConcepts.jsx';
 import Header from './Components/Header/Header.jsx';
-import TabButton from './Components/TabButton/TabButton.jsx';
+import Examples from './Components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-
-  function handleSelect(selectedButton) {
-    // seletced button should be a string => 'Components, JSX, props, state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
 
   return (
-    <div>
+    <>                  {/*  <-- new fragment component to remove unnecessary div in dom */}
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts!</h2>
-          <ul>
-            
-            {/* <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept{...CORE_CONCEPTS[1]} />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept{...CORE_CONCEPTS[3]} /> */}
-
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem}/>
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-
-          {/* :null is the else case it can also be written the other way with no else case. */}
-          
-          {!selectedTopic ? <p>Please select a topic</p> : null}
-          {selectedTopic && <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>}
-
-        </section>
+        <CoreConcepts/> {/* <-- added a feature page to this component ( idk is this too much abstraction) I will leave the examples section for now */}
+        <Examples/>
       </main>
-    </div>
+    </>
   );
 }
 
